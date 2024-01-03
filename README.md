d8becc8f04a46e3a5c69c7d83e4b098c24f6b4bc
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-01-03 16:15:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CRUD Simples mysql
+
---
+++ src/test/java/com/java/contatos/CRUDMYSQL/AppTest.java
@@ -0,0 +1,20 @@
+package com.java.contatos.CRUDMYSQL;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
